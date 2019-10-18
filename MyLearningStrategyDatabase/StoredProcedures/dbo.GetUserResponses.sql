6001a561582a39b0eeacdde08ba827c3e92ea805
Local WriterOfCode <writerofcode@hotmail.com>
2019-10-18 08:43:27 -0400

    #

--- MyLearningStrategyDatabase/StoredProcedures/dbo.GetUserResponses.sql
+++ MyLearningStrategyDatabase/StoredProcedures/dbo.GetUserResponses.sql
@@ -5,64 +5,7 @@
 	@Originator UNIQUEIDENTIFIER
 AS
 
-IF (@BodyOfKnowledgeId IS NULL AND @QuestionId IS NULL AND @ResponseId IS NULL )
-BEGIN
-	SELECT r.ResponseId
-			,r.QuestionId
-			,r.OrderBy
-			,r.IsCorrect
-			,r.Response
-			,r.Image_1_Device
-			,r.Image_1_Cloud
-			,r.Image_1_Hash
-			,r.Image_2_Device
-			,r.Image_2_Cloud
-			,r.Image_2_Hash
-			,r.Image_3_Device
-			,r.Image_3_Cloud
-			,r.Image_3_Hash
-			,r.Hyperlink_1
-			,r.Hyperlink_2
-			,r.Hyperlink_3
-			,r.Mnemonic
-			,r.LastModifiedOffset
-			,r.CloudRowId
-		FROM dbo.Responses r
-		join Questions q on r.QuestionId = q.QuestionId
-		join BodyOfKnowledge b on b.BodyOfKnowledgeId = q.BodyOfKnowledgeId
-		join UserProfiles u on b.UserProfileId = u.UserProfileId
-		WHERE  u.Originator = @Originator
-END 
-ELSE IF (@BodyOfKnowledgeId IS NOT NULL AND @QuestionId IS NULL AND @ResponseId IS NULL )
-BEGIN
-	SELECT r.ResponseId
-			,r.QuestionId
-			,r.OrderBy
-			,r.IsCorrect
-			,r.Response
-			,r.Image_1_Device
-			,r.Image_1_Cloud
-			,r.Image_1_Hash
-			,r.Image_2_Device
-			,r.Image_2_Cloud
-			,r.Image_2_Hash
-			,r.Image_3_Device
-			,r.Image_3_Cloud
-			,r.Image_3_Hash
-			,r.Hyperlink_1
-			,r.Hyperlink_2
-			,r.Hyperlink_3
-			,r.Mnemonic
-			,r.LastModifiedOffset
-			,r.CloudRowId
-		FROM dbo.Responses r
-		join Questions q on r.QuestionId = q.QuestionId
-		join BodyOfKnowledge b on b.BodyOfKnowledgeId = q.BodyOfKnowledgeId
-		join UserProfiles u on b.UserProfileId = u.UserProfileId
-		WHERE  u.Originator = @Originator
-	    AND b.BodyOfKnowledgeId = @BodyOfKnowledgeId
-END
-ELSE IF (@QuestionId IS NOT NULL AND @ResponseId IS NULL )
+IF (@Originator IS NOT NULL AND  @BodyOfKnowledgeId IS NOT NULL AND @QuestionId IS NOT NULL AND @ResponseId IS NOT NULL )
 BEGIN
 	SELECT r.ResponseId
 			,r.QuestionId
@@ -91,8 +34,9 @@ BEGIN
 		WHERE  u.Originator = @Originator
 	    AND b.BodyOfKnowledgeId = @BodyOfKnowledgeId
 		AND R.QuestionId= @QuestionId
-END
-ELSE
+	    AND R.ResponseId= @ResponseId;
+END 
+ELSE IF (@Originator IS NOT NULL AND  @BodyOfKnowledgeId IS NOT NULL AND @QuestionId IS NOT NULL AND @ResponseId IS NULL )
 BEGIN
 
 	SELECT r.ResponseId
@@ -121,10 +65,64 @@ BEGIN
 		join UserProfiles u on b.UserProfileId = u.UserProfileId
 		WHERE  u.Originator = @Originator
 	    AND b.BodyOfKnowledgeId = @BodyOfKnowledgeId
-		AND R.QuestionId= @QuestionId
-	    AND R.ResponseId= @ResponseId
+		AND R.QuestionId= @QuestionId;
 END 
-
-
+ELSE IF (@Originator IS NOT NULL AND  @BodyOfKnowledgeId IS NOT NULL AND @QuestionId IS NULL AND @ResponseId IS NULL )
+BEGIN
+	SELECT r.ResponseId
+			,r.QuestionId
+			,r.OrderBy
+			,r.IsCorrect
+			,r.Response
+			,r.Image_1_Device
+			,r.Image_1_Cloud
+			,r.Image_1_Hash
+			,r.Image_2_Device
+			,r.Image_2_Cloud
+			,r.Image_2_Hash
+			,r.Image_3_Device
+			,r.Image_3_Cloud
+			,r.Image_3_Hash
+			,r.Hyperlink_1
+			,r.Hyperlink_2
+			,r.Hyperlink_3
+			,r.Mnemonic
+			,r.LastModifiedOffset
+			,r.CloudRowId
+		FROM dbo.Responses r
+		join Questions q on r.QuestionId = q.QuestionId
+		join BodyOfKnowledge b on b.BodyOfKnowledgeId = q.BodyOfKnowledgeId
+		join UserProfiles u on b.UserProfileId = u.UserProfileId
+		WHERE  u.Originator = @Originator
+	    AND b.BodyOfKnowledgeId = @BodyOfKnowledgeId
+END
+ELSE IF (@Originator IS NOT NULL AND  @BodyOfKnowledgeId IS NULL AND @QuestionId IS NULL AND @ResponseId IS NULL )
+BEGIN
+	SELECT r.ResponseId
+			,r.QuestionId
+			,r.OrderBy
+			,r.IsCorrect
+			,r.Response
+			,r.Image_1_Device
+			,r.Image_1_Cloud
+			,r.Image_1_Hash
+			,r.Image_2_Device
+			,r.Image_2_Cloud
+			,r.Image_2_Hash
+			,r.Image_3_Device
+			,r.Image_3_Cloud
+			,r.Image_3_Hash
+			,r.Hyperlink_1
+			,r.Hyperlink_2
+			,r.Hyperlink_3
+			,r.Mnemonic
+			,r.LastModifiedOffset
+			,r.CloudRowId
+		FROM dbo.Responses r
+		join Questions q on r.QuestionId = q.QuestionId
+		join BodyOfKnowledge b on b.BodyOfKnowledgeId = q.BodyOfKnowledgeId
+		join UserProfiles u on b.UserProfileId = u.UserProfileId
+		WHERE  u.Originator = @Originator;
+END
 
 RETURN 0
