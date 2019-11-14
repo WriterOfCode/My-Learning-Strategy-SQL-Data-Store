85616f9707c5f42f2289062a30de9f0d85d44047
Local WriterOfCode <writerofcode@hotmail.com>
2019-11-13 23:30:41 -0500

    #altered predeploy script

--- MyLearningStrategyDatabase/dbo.AlterDataPreInstall.sql
+++ MyLearningStrategyDatabase/dbo.AlterDataPreInstall.sql
@@ -6,13 +6,13 @@
 --UPDATE [dbo].[LearningStrategies] 
 --SET [Name] ='Default'
 --WHERE [Name] IS NULL;
-IF OBJECT_ID (N'LearningStrategies',N'U') IS NOT NULL
-BEGIN
-	DELETE FROM [LearningStrategies]
-	DROP TABLE [LearningStrategies]
-END 
-GO
-UPDATE [dbo].[BodyOfKnowledge]
-SET [Name] = 'Default'
-WHERE [Name] IS NULL;
-GO
+--IF OBJECT_ID (N'LearningStrategies',N'U') IS NOT NULL
+--BEGIN
+--	DELETE FROM [LearningStrategies]
+--	DROP TABLE [LearningStrategies]
+--END 
+--GO
+--UPDATE [dbo].[BodyOfKnowledge]
+--SET [Name] = 'Default'
+--WHERE [Name] IS NULL;
+--GO
