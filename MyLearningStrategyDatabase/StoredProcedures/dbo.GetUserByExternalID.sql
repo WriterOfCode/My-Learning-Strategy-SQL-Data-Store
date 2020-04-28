a4780d44acbf61e7fc0b0b66bf92a8a468da9b8f
Local WriterOfCode <writerofcode@hotmail.com>
2020-04-28 16:50:52 -0400

    #New User

--- MyLearningStrategyDatabase/StoredProcedures/dbo.GetUserByExternalID.sql
+++ MyLearningStrategyDatabase/StoredProcedures/dbo.GetUserByExternalID.sql
@@ -1,48 +1,21 @@
 ﻿CREATE PROCEDURE [dbo].[GetUserByExternalID]
 	@ExternalID  NVARCHAR (450),
-	@DisplayName  NVARCHAR (256) NULL,
-	@EmailAddress       NVARCHAR (500) NULL,
-	@FirstName   NVARCHAR (256) NULL,
-	@LastName    NVARCHAR (256) NULL,
-	@PostalCode NCHAR(10) NULL, 
-	@IdentityProvider NVARCHAR(2083) NULL, 
-	@ImageDevice NVARCHAR(256) NULL, 
-	@ImageCloud NVARCHAR(2083) NULL,
-	@ImageHash INT NULL, 
-	@HasLoggedIn BIT NULL,
-	@IsLocked    BIT NULL, 
-	@IsDisabled  BIT NULL, 
-	@IsDeleted BIT NULL
+	@IdentityProvider NVARCHAR(2083) NULL,
+	@DisplayName  NVARCHAR (256) NULL
 AS	
 
 
-	IF EXISTS (	SELECT * FROM UserProfiles WHERE ExternalID = @ExternalID)
-		BEGIN
-			SELECT UserProfileId,ExternalID,DisplayName,EmailAddress,
-					FirstName,LastName,PostalCode,IdentityProvider,
-					Originator,ImageDevice,ImageCloud,ImageHash,
-					HasLoggedIn,IsLocked,IsDisabled,IsDeleted,
-					LastModifiedOffset
-			FROM UserProfiles 
-			WHERE ExternalID = @ExternalID;
-		END
-	ELSE
-		BEGIN
-		INSERT INTO UserProfiles
-		(ExternalID,DisplayName,EmailAddress,FirstName,LastName,
-		PostalCode,IdentityProvider,ImageDevice,
-		ImageCloud,ImageHash,HasLoggedIn,IsLocked,IsDisabled,
-		IsDeleted)
-	VALUES (@ExternalID,@DisplayName,@EmailAddress,@FirstName,
-    @LastName,@PostalCode,@IdentityProvider,
-	@ImageDevice,@ImageCloud,@ImageHash,@HasLoggedIn,
-    @IsLocked,@IsDisabled,@IsDeleted);
+	IF NOT EXISTS (	SELECT * FROM UserProfiles WHERE ExternalID = @ExternalID AND IdentityProvider=@IdentityProvider)
+	BEGIN
+		INSERT INTO UserProfiles (ExternalID,DisplayName,IdentityProvider)
+		VALUES (@ExternalID,@DisplayName,@IdentityProvider);
+	END
 
-		SELECT UserProfileId,ExternalID,DisplayName,EmailAddress,
-					FirstName,LastName,PostalCode,IdentityProvider,
-					Originator,ImageDevice,ImageCloud,ImageHash,
-					HasLoggedIn,IsLocked,IsDisabled,IsDeleted
-		FROM UserProfiles 
-		WHERE ExternalID = @ExternalID;
-		END
+	SELECT UserProfileId,ExternalID,DisplayName,EmailAddress,
+	FirstName,LastName,PostalCode,IdentityProvider,
+	Originator,ImageDevice,ImageCloud,ImageHash,
+	HasLoggedIn,IsLocked,IsDisabled,IsDeleted,
+	LastModifiedOffset
+	FROM UserProfiles 
+	WHERE ExternalID = @ExternalID;
 RETURN
