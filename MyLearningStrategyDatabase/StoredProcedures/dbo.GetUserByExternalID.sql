d2e0790a0f284c7190997b8c01157b22c03746d6
WriterOfCode <daviddavis@writerofcode.net>
2021-10-03 17:19:39 -0400

    Added default category and strategy

--- MyLearningStrategyDatabase/StoredProcedures/dbo.GetUserByExternalID.sql
+++ MyLearningStrategyDatabase/StoredProcedures/dbo.GetUserByExternalID.sql
@@ -8,8 +8,29 @@ AS
 
 	IF NOT EXISTS (	SELECT * FROM UserProfiles WHERE ExternalID = @ExternalID AND IdentityProvider=@IdentityProvider)
 	BEGIN
+		Declare @UserProfileId int;
+
 		INSERT INTO UserProfiles (ExternalID,DisplayName,IdentityProvider,FirstName)
 		VALUES (@ExternalID,@DisplayName,@IdentityProvider,@FirstName);
+		SET @UserProfileId = CAST(SCOPE_IDENTITY() AS INT);
+
+		commit;
+		
+		INSERT INTO [dbo].[Categories](UserProfileId,CategoryName,ImageDevice,ImageCloud,ImageHash )
+		VALUES (@UserProfileId,'Default','Default','Default',0);
+ 	
+		INSERT INTO [dbo].[Strategies]
+			(Name,
+			UserProfileId, 
+			Description, 
+			SortRuleId,  
+			QuestionSelection,
+			ResponseSelection,
+			OnlyCorrect,
+			RecycleIncorrectlyAnswered)
+		VALUES ('Default',@UserProfileId, 'Default', 0, 0, 0, 0,0);
+
+
 	END
 
 	SELECT UserProfileId,ExternalID,DisplayName,EmailAddress,
